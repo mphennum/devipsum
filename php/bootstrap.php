f7b7968a9d4c124ac6e90145e6aa91efa498179d
Max Hennum <mphennum@gmail.com>
2014-08-08 16:01:18 +0000

    added handlers, config, database to bootstrap

--- php/bootstrap.php
+++ php/bootstrap.php
@@ -2,9 +2,17 @@
 
 namespace DevIpsum;
 
+require __DIR__ . '/Config.php';
+require __DIR__ . '/Database.php';
 require __DIR__ . '/DevIpsum.php';
 require __DIR__ . '/Handler.php';
 require __DIR__ . '/Response.php';
 require __DIR__ . '/Request.php';
+
+require __DIR__ . '/handlers/api/User.php';
+require __DIR__ . '/handlers/api/Text.php';
+
+require __DIR__ . '/handlers/www/Docs.php';
+require __DIR__ . '/handlers/www/Home.php';
 
 DevIpsum::init();
