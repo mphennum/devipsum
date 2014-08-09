8d69ed2f86d3b807af1a7c505dba77a19abc7d2e
Max Hennum <mphennum@gmail.com>
2014-08-09 15:57:53 +0000

    controller: handling CORS and origins

--- php/DevIpsum.php
+++ php/DevIpsum.php
@@ -8,37 +8,56 @@ abstract class DevIpsum {
 	}
 
 	static public function handle() {
+		// host
 		$host = null;
 		if (isset($_SERVER['HTTP_HOST'])) {
 			$host = $_SERVER['HTTP_HOST'];
 		}
 
+		// method
 		$method = null;
 		if (isset($_SERVER['REQUEST_METHOD'])) {
 			$method = $_SERVER['REQUEST_METHOD'];
 		}
 
-		if ($method === 'OPTIONS') {
-			// options headers, allow all
+		// headers
+		if (isset($_SERVER['HTTP_ORIGIN'])) {
+			header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+			//header(Access-Control-Allow-Headers: ');
+			//header(Access-Control-Expose-Headers: ');
+			//header('Access-Control-Allow-Credentials: true']);
 		}
 
+		// CORS
+		if ($method === 'OPTIONS') {
+			header('Access-Control-Max-Age: 86400'); // 1 day
+
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+				header('Access-Control-Allow-Methods: GET, OPTIONS');
+			}
+		}
+
+		// ip
 		$ip = null;
 		if (isset($_SERVER['REMOTE_ADDR'])) {
 			$ip = $_SERVER['REMOTE_ADDR'];
 		}
 
+		// user agent
 		$agent = null;
 		if (isset($_SERVER['HTTP_USER_AGENT'])) {
 			$agent = $_SERVER['HTTP_USER_AGENT'];
 		}
 
+		// params
 		$params = [];
 		if ($method === 'GET') {
 			$params = $_GET;
 		}
 
+		// request
 		$resource = null;
-		$format = null;
+		$format = 'json';
 		if (isset($_SERVER['REQUEST_URI'])) {
 			$request = $_SERVER['REQUEST_URI'];
 			$request = preg_replace('/\?.*$/', '', $request);
@@ -50,14 +69,15 @@ abstract class DevIpsum {
 			$resource = $request[0];
 
 			$len = count($request);
-			if ($len === 1) {
-				$format = 'json';
-			} else {
+			if ($len > 1) {
 				$format = $request[$len - 1];
 			}
 		}
 
-		//$handler = Handler::factory($method, $resource, $params, $format)
-		//$handler->handle();
+		// handler
+		$handler = Handler::factory($host, $method, $resource, $params, $format);
+		$handler->handle();
+
+		// view
 	}
 }
