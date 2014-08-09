f37900520c2412a919b7c224d56957b58fad3c4b
Max Hennum <mphennum@gmail.com>
2014-08-09 15:56:40 +0000

    handlers: constructors calling parents

--- php/handlers/www/Docs.php
+++ php/handlers/www/Docs.php
@@ -6,8 +6,8 @@ use DevIpsum\Handler;
 
 class Docs extends Handler {
 
-	public function __construct() {
-		parent::__construct();
+	public function __construct($method, $resource, $params, $format) {
+		parent::__construct($method, $resource, $params, $format);
 	}
 
 	public function handle() {
--- php/handlers/www/Home.php
+++ php/handlers/www/Home.php
@@ -6,11 +6,10 @@ use DevIpsum\Handler;
 
 class Home extends Handler {
 
-	public function __construct() {
-		parent::__construct();
+	public function __construct($method, $resource, $params, $format) {
+		parent::__construct($method, $resource, $params, $format);
 	}
 
 	public function handle() {
-
 	}
 }
