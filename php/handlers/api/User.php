22522aab385ecb36c0cf268b35a93c458712c5b1
Max Hennum <mphennum@gmail.com>
2014-08-10 07:52:47 +0000

    api: basic users with first, last, gender

--- php/handlers/api/User.php
+++ php/handlers/api/User.php
@@ -3,6 +3,7 @@
 namespace DevIpsum\Handlers\API;
 
 use DevIpsum\Handler;
+use DevIpsum\Database;
 
 class User extends Handler {
 
@@ -12,5 +13,51 @@ class User extends Handler {
 
 	public function handle() {
 		parent::handle();
+
+		$names = [
+			'first' => Database::read('names', 'type = :type', [':type' => 'first']),
+			'last' => Database::read('names',  'type = :type', [':type' => 'last'])
+		];
+
+		$maxFirst = count($names['first']) - 1;
+		$maxLast = count($names['last']) - 1;
+		$users = [];
+
+		$n = (isset($this->params['n']) ? $this->params['n'] : 1);
+
+		for ($i = 0; $i < $n; ++$i) {
+			$first = $names['first'][mt_rand(0, $maxFirst)];
+
+			$users[] = [
+				'name' => [
+					'first' => $first['name'],
+					'last' => $names['last'][mt_rand(0, $maxLast)]['name']
+				],
+				'gender' => $first['gender'],
+				'born' => '',
+				'age' => '',
+				'address' => [
+					'street' => '',
+					'city' => '',
+					'state' => '',
+					'country' => '',
+					'zip' => ''
+				],
+				'contact' => [
+					'phone' => '',
+					'email' => '',
+					'social' => '',
+					'website' => ''
+				],
+				'email' => '',
+				'phone' => '',
+				'job' => [
+					'title' => '',
+					'salary' => ''
+				]
+			];
+		}
+
+		$this->response->users = $users;
 	}
 }
