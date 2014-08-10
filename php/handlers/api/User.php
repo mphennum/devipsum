d27105ca178c5bc58a8739837595bd2a5540ea90
Max Hennum <mphennum@gmail.com>
2014-08-10 19:38:12 +0000

    user birth date, age, full name, email

--- php/handlers/api/User.php
+++ php/handlers/api/User.php
@@ -2,6 +2,9 @@
 
 namespace DevIpsum\Handlers\API;
 
+use DateTime;
+
+use DevIpsum\DevIpsum;
 use DevIpsum\Handler;
 use DevIpsum\Database;
 
@@ -23,20 +26,33 @@ class User extends Handler {
 		$maxLast = count($names['last']) - 1;
 		$users = [];
 
-		$n = (isset($this->params['n']) ? $this->params['n'] : 1);
+		$now = new DateTime('now', DevIpsum::$dtz);
+		$now = $now->getTimestamp();
 
+		$daysInYear = 60 * 60 * 24 * 365.2425;
+
+		$n = (isset($this->params['n']) ? $this->params['n'] : 1);
 		for ($i = 0; $i < $n; ++$i) {
 			$first = $names['first'][mt_rand(0, $maxFirst)];
 			$last = $names['last'][mt_rand(0, $maxLast)];
 
+			$ts = mt_rand(-631151999, 946684800); // 1950 to 2000
+			$date = new DateTime('now', DevIpsum::$dtz);
+			$date->setTimestamp($ts);
+			$age = (int) (($now - $ts) / ($daysInYear));
+
 			$users[] = [
 				'name' => [
 					'first' => $first['name'],
-					'last' => $last['name']
+					'last' => $last['name'],
+					'full' => $first['name'] . ' ' . $last['name']
 				],
 				'gender' => $first['gender'],
-				'born' => '',
-				'age' => '',
+				'birth' => [
+					'date' => $date->format('c'),
+					'age' => $age,
+					'ts' => $ts
+				],
 				'address' => [
 					'street' => '',
 					'city' => '',
@@ -46,15 +62,13 @@ class User extends Handler {
 				],
 				'contact' => [
 					'phone' => '',
-					'email' => '',
-					'social' => '',
+					'email' => strtolower($first['name']{0} . $last['name']) . '@gmail.com',
+					'social' => [
+						'google' => '',
+						'facebook' => '',
+						'twitter' => ''
+					],
 					'website' => ''
-				],
-				'email' => '',
-				'phone' => '',
-				'job' => [
-					'title' => '',
-					'salary' => ''
 				]
 			];
 		}
