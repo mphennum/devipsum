ee8f2916851125bc9871fcee3c3ab43d20f3dea8
Max Hennum <mphennum@gmail.com>
2014-08-16 21:21:27 +0000

    added google analytics to html views

--- php/views/home.html.php
+++ php/views/home.html.php
@@ -20,9 +20,13 @@
 <main>
 	<p>source: <a href="//github.com/mphennum/devipsum/">github.com/mphennum/devipsum</a></p>
 	<p>docs: <a href="//github.com/mphennum/devipsum/wiki/">github.com/mphennum/devipsum/wiki</a></p>
-	<h2>Example</h2>
+	<h2>Examples</h2>
 	<p>request: <a href="http://api.devipsum.com/user.json?n=10">api.devipsum.com/user.json?n=10</a></p>
+	<p>request: <a href="http://api.devipsum.com/text.json?n=5">api.devipsum.com/text.json?n=5</a></p>
 </main>
+
+
+<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','UA-53886971-1','auto');ga('send','pageview');</script>
 
 </body>
 
