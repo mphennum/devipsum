ffdbd3cf9ddda5f59ea6b2ad3b4179924f7c2079
Max Hennum <mphennum@gmail.com>
2014-08-17 19:31:23 +0000

    min.css for live mode

--- lib/views/ssi/styles.html.php
+++ lib/views/ssi/styles.html.php
@@ -1,3 +1,9 @@
 <link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet'>
+
+<?php if (DevIpsum\Config::DEV_MODE) { ?>
 <link href="//css.devipsum.com/src/main.css" rel="stylesheet">
+<?php } else { ?>
+<link href="//css.devipsum.com/min.css" rel="stylesheet">
+<?php } ?>
+
 <link rel="shortcut icon" href="//img.devipsum.com/favicon.png">
