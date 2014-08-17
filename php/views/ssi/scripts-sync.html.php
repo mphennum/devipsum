ba4b09f548f9c2338b049c0b0ec55ba65b4e1e8d
Max Hennum <mphennum@gmail.com>
2014-08-17 01:22:47 +0000

    ssi files

---
+++ php/views/ssi/scripts-sync.html.php
@@ -0,0 +1,5 @@
+<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
---
+++ php/views/ssi/styles.html.php
@@ -0,0 +1,2 @@
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet'>
+<link href="//css.devipsum.com/src/main.css" rel="stylesheet">
