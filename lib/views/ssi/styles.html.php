020cc74a5952afed2a70916b1a487c24ff4da30d
Max Hennum <mphennum@gmail.com>
2014-08-17 19:14:38 +0000

    added simple favicon png

--- lib/views/ssi/styles.html.php
+++ lib/views/ssi/styles.html.php
@@ -1,2 +1,3 @@
 <link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet'>
 <link href="//css.devipsum.com/src/main.css" rel="stylesheet">
+<link rel="shortcut icon" href="//img.devipsum.com/favicon.png">
